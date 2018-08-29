8474ef0025d6fdbd0449658439518a0f48eba29a
Nolan Neeley <nolanneeley@gmail.com>
2018-08-29 15:42:48 -0500

    added a toggle complete button with input for position. When clicked the button fires the toggleCompleted function on the handlers object, passes the position number from the input box, and toggles the todo either complete or incomplete depending on its previous state.

--- script.js
+++ script.js
@@ -62,9 +62,6 @@ var handlers = {
   displayTodos: function() {
     todoList.displayTodos();
   },
-  toggleAll: function() {
-    todoList.toggleAll();
-  },
   addTodo: function() {
     var addTodoTextInput = document.getElementById('addTodoTextInput');
     todoList.addTodo(addTodoTextInput.value);
@@ -81,5 +78,13 @@ var handlers = {
     var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
     todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
     deleteTodoPositionInput.value = "";
+  },
+  toggleCompleted: function() {
+    var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+    todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+    toggleCompletedPositionInput.value = "";
+  },
+  toggleAll: function() {
+    todoList.toggleAll();
   }
 };
